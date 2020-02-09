e29ce71c81b05f768e98f3d8a81f6824b14eebf8
harounchahed <haroun.chd@gmail.com>
2020-02-09 17:23:11 +0800

    time dependenet covariates (PF ratio) for MSM

---
+++ Haroun/Code/Production/Marginal Structural Models/TIME_DEP_CONF.sql
@@ -0,0 +1,26 @@
+SELECT
+  c.subject_id,
+  c.hadm_id,
+  c.icustay_id,
+  c.mv_durations_hours,
+  c.mv_starttime,
+  c.mv_endtime,
+  bgfd.pao2fio2,
+  bgfd.charttime AS pf_charttime,
+  -- we select all PF ratios taken during the mechanical ventilation period along with their timestamps
+FROM
+  `ync-capstones.NMB.COHORT_BASELINE_P2` c
+LEFT OUTER JOIN
+  `ync-capstones.MIMIC_V1_4_derived.bloodgasfirstdayarterial` bgfd
+ON
+  c.subject_id = bgfd.subject_id
+  AND c.hadm_id = bgfd.hadm_id
+  AND c.icustay_id = bgfd.icustay_id
+WHERE
+  c.mv_starttime < bgfd.charttime
+  AND c.mv_endtime > bgfd.charttime
+  AND bgfd.pao2fio2 IS NOT NULL
+ORDER BY
+  c.subject_id,
+  c.hadm_id,
+  c.icustay_id
