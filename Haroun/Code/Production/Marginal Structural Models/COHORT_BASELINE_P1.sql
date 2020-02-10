df8d48d8e34d1d914526837158cb60b129d7b0e9
harounchahed <haroun.chd@gmail.com>
2020-02-10 23:25:47 +0800

    Fixed time comparison signs for consistency

--- Haroun/Code/Production/Marginal Structural Models/COHORT_BASELINE_P1.sql
+++ Haroun/Code/Production/Marginal Structural Models/COHORT_BASELINE_P1.sql
@@ -260,14 +260,16 @@ WITH
     AND mv.hadm_id = c.hadm_id
     AND mv.icustay_id = c.icustay_id
   WHERE
+  -- PF ratio time constraits
     datetime_diff(mv.mv_starttime,
       bgfd.charttime,
       HOUR) <= 24
-    AND mv.mv_starttime < bgfd.charttime
+    AND mv.mv_starttime <= bgfd.charttime
+  -- PEEP time constraints
     AND datetime_diff(mv.mv_starttime,
       c.charttime,
       HOUR) <= 24
-    AND mv.mv_starttime < c.charttime
+    AND mv.mv_starttime <= c.charttime
     AND c.itemid IN (60,
       437,
       505,
